2d6dea04acba425e0c23eed908419d2a836adbcb
Jing Qian <qj883107@hotmail.com>
2017-08-15 23:55:37 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -23,5 +23,5 @@ with(PowerFeb, {
     lines(DateTime, Sub_metering_3, col = 'Blue')
     legend("topright", col = c("black", "red", "blue"), lwd = c(1,1,1), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 })
-# dev.copy(png, "plot3.png", height = 480, width = 480)
-# dev.off()
+dev.copy(png, "plot3.png", height = 480, width = 480)
+dev.off()
