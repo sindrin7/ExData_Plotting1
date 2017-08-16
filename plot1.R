5ca684376b314695809f916b7e4a40e72b5d0728
Jing Qian <qj883107@hotmail.com>
2017-08-15 23:56:12 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -18,5 +18,5 @@ PowerFeb$DateTime <- strptime(paste(PowerFeb$Date, PowerFeb$Time), "%Y-%m-%d %H:
 
 ### PLOT 1   
 hist(PowerFeb$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
-# dev.copy(png, "plot1.png", width = 480, height = 480)
-# dev.off()
+dev.copy(png, "plot1.png", width = 480, height = 480)
+dev.off()
